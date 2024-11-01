22f6c8403ff1ed88a5e2762a49013606a703efc3
gunnnj <toanquoc2003@gmail.com>
2024-11-01 23:12:18 +0700

    a

---
+++ LaserScript.cs
@@ -0,0 +1,99 @@
+﻿/*
+* Copyright (c) 2018 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+* distribute, sublicense, create a derivative work, and/or sell copies of the
+* Software in any work that is designed, intended, or marketed for pedagogical or
+* instructional purposes related to programming, coding, application development,
+* or information technology.  Permission for such use, copying, modification,
+* merger, publication, distribution, sublicensing, creation of derivative works,
+* or sale is expressly withheld.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    //1
+    public Sprite laserOnSprite;
+
+    public Sprite laserOffSprite;
+
+    //2
+    public float toggleInterval = 0.5f;
+
+    public float rotationSpeed = 0.0f;
+
+    //3
+    private bool isLaserOn = true;
+
+    private float timeUntilNextToggle;
+
+    //4
+    private Collider2D laserCollider;
+
+    private SpriteRenderer laserRenderer;
+
+    // Use this for initialization
+    private void Start()
+    {
+        //1
+        timeUntilNextToggle = toggleInterval;
+        //2
+        laserCollider = gameObject.GetComponent<Collider2D>();
+        laserRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //1
+        timeUntilNextToggle -= Time.deltaTime;
+
+        //2
+        if (timeUntilNextToggle <= 0)
+        {
+            //3
+            isLaserOn = !isLaserOn;
+
+            //4
+            laserCollider.enabled = isLaserOn;
+
+            //5
+            if (isLaserOn)
+            {
+                laserRenderer.sprite = laserOnSprite;
+            }
+            else
+            {
+                laserRenderer.sprite = laserOffSprite;
+            }
+
+            //6
+            timeUntilNextToggle = toggleInterval;
+        }
+
+        //7
+        transform.RotateAround(transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
